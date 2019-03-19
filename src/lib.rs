bd70f1d58dfbcaf662c4aea11b2eb2c3d1c29bc8
ponyloop <an.laaakso@gmail.com>
2019-03-19 23:08:20 +0200

    Benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -44,9 +44,8 @@ pub fn sorensen<T: Sized + Hash + Eq>(first: &[T], second: &[T]) -> f64 {
 #[cfg(test)]
 mod tests {
     extern crate rand;
-
     use super::*;
-    use test::Bencher;
+
 
     #[test]
     fn sorensen_distance_test() {
@@ -82,107 +81,5 @@ mod tests {
     fn sorensen_distance_exact_test() {
         let result = sorensen("night".as_bytes(), "night".as_bytes());
         assert_float_absolute_eq!(result, 1f64)
-    }
-
-
-    #[bench]
-    fn intersection_2sym_bench(b: &mut Bencher) {
-        b.iter(|| {
-            sorensen(
-                "ac".as_bytes(),
-                "bc".as_bytes()
-            )
-        })
-    }
-
-    #[bench]
-    fn intersection_20sym_bench(b: &mut Bencher) {
-        let str1 = (0..20).map(|_| { rand::random::<char>() }).collect::<String>();
-        let str2 = (0..20).map(|_| { rand::random::<char>() }).collect::<String>();
-
-        b.iter(|| {
-            sorensen(
-                str1.as_bytes(),
-                str2.as_bytes()
-            )
-        })
-    }
-
-    #[bench]
-    fn intersection_200_long_bench(b: &mut Bencher) {
-        let str1 = (0..200).map(|_| { rand::random::<char>() }).collect::<String>();
-        let str2 = (0..200).map(|_| { rand::random::<char>() }).collect::<String>();
-
-        b.iter(|| {
-            sorensen(
-                str1.as_str().as_bytes(),
-                str2.as_str().as_bytes()
-            )
-        })
-    }
-
-    #[bench]
-    fn intersection_2000_long_bench(b: &mut Bencher) {
-        let str1 = (0..2000).map(|_| { rand::random::<char>() }).collect::<String>();
-        let str2 = (0..2000).map(|_| { rand::random::<char>() }).collect::<String>();
-
-        b.iter(|| {
-            sorensen(
-                str1.as_str().as_bytes(),
-                str2.as_str().as_bytes()
-            )
-        })
-    }
-
-    #[bench]
-    fn intersection_20000_long_bench(b: &mut Bencher) {
-        let str1 = (0..20000).map(|_| { rand::random::<char>() }).collect::<String>();
-        let str2 = (0..20000).map(|_| { rand::random::<char>() }).collect::<String>();
-
-        b.iter(|| {
-            sorensen(
-                str1.as_str().as_bytes(),
-                str2.as_str().as_bytes()
-            )
-        })
-    }
-
-    #[bench]
-    fn intersection_200000_long_bench(b: &mut Bencher) {
-        let str1 = (0..200000).map(|_| { rand::random::<char>() }).collect::<String>();
-        let str2 = (0..200000).map(|_| { rand::random::<char>() }).collect::<String>();
-
-        b.iter(|| {
-            sorensen(
-                str1.as_str().as_bytes(),
-                str2.as_str().as_bytes()
-            )
-        })
-    }
-
-    #[bench]
-    fn intersection_2000000_long_bench(b: &mut Bencher) {
-        let str1 = (0..2_000_000).map(|_| { rand::random::<char>() }).collect::<String>();
-        let str2 = (0..2_000_000).map(|_| { rand::random::<char>() }).collect::<String>();
-
-        b.iter(move || {
-            sorensen(
-                str1.as_str().as_bytes(),
-                str2.as_str().as_bytes()
-            )
-        })
-    }
-
-    #[bench]
-    fn intersection_1000000_long_vec_bench(b: &mut Bencher) {
-        let str1 = (0..1000000).map(|_| { rand::random::<u64>() }).collect::<Vec<u64>>();
-        let str2 = (0..1000000).map(|_| { rand::random::<u64>() }).collect::<Vec<u64>>();
-
-        b.iter(move || {
-            sorensen(
-                &str1,
-                &str2
-            )
-        })
     }
 }
